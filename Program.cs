40f30c605265c2d73ecf3fd80ac8aa2bf812382d
ronxss <ronsilva768@gmail.com>
2021-11-23 09:31:13 -0300

    Exercícios Aula 2

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Exercício 1:");
+            ////declarando variáveis com valores iniciais
+            //int i = 10;
+            //long l = 20;
+            //float f = 7.5f;
+            //double d = 7.21;
+            //decimal de = 0.5m;
+
+            ////retornando variáveis com valores iniciais
+            //Console.WriteLine("Int: " + i);
+            //Console.WriteLine("Long: " + l);
+            //Console.WriteLine("Float: " + f);
+            //Console.WriteLine("Double: " + d);
+            //Console.WriteLine("Decimal: " + de);
+
+            ////declarando variáveis com valores de entrada pelo usuário
+            //i = int.Parse(Console.ReadLine());
+            //l = long.Parse(Console.ReadLine());
+            //f = float.Parse(Console.ReadLine());
+            //d = double.Parse(Console.ReadLine());
+            //de = decimal.Parse(Console.ReadLine());
+
+            ////retornando variáveis com valores dados pelo usuário
+            //Console.WriteLine("Int: " + i);
+            //Console.WriteLine("Long: " + l);
+            //Console.WriteLine("Float: " + f);
+            //Console.WriteLine("Double: " + d);
+            //Console.WriteLine("Decimal: " + de);
+
+            //Console.WriteLine("Exercício 2: ");
+
+            ////declarando variáves byte e sbyte com valores iniciais
+
+            ////   byte b = 256;
+            ////   sbyte s = 128;
+
+            //Console.WriteLine("Exercício 3: ");
+            //short sh = 10;
+            //uint u = 50;
+            //l = 30;
+            //ulong ul = 70;
+
+            //Console.WriteLine("Short: " + sh);
+            //Console.WriteLine("Uint: " + u);
+            //Console.WriteLine("Long: " + l);
+            //Console.WriteLine("Ulong: " + ul);
+
+
+            //Console.WriteLine("Exercício 4: ");
+
+            //string frase = Console.ReadLine();
+            //char[] chars = frase.ToCharArray();
+
+            //for (int i = 0; i < chars.Length; i++)
+            //{
+            //    if(chars[i] == 'g')
+            //    {
+            //        Console.WriteLine("Encontrei a letra G");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Não encontrei a letra G");
+            //    }
+
+            //}
+
+            //Console.WriteLine("Exercício 5: ");
+
+            //Console.WriteLine("Digite um número inteiro");
+            //int i = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite uma frase");
+            //string s = Console.ReadLine();
+            //Console.WriteLine("Digite um caractere qualquer");
+            //char c = char.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite um número decimal");
+            //decimal d = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite um valor no formato de moeda (0.00)");
+            //double dbl = double.Parse(Console.ReadLine());
+
+            //object[] arr = { i, s, c, d, dbl };
+
+            //for (int n = 0; i < arr.Length; n++)
+            //{
+            //    Console.WriteLine(arr[n]);
+            //}
+
+            //Console.WriteLine("Exercício 6: ");
+
+            //int i;
+            //float f = 32.5f;
+
+            //Convert.ToInt32(f);
+            //Console.WriteLine(f);
+            //int.Parse(f);
+            //Console.WriteLine(f);
+
+            //Console.WriteLine("Exercício 7: ");
+
+            //for(int i = 1; i<=10; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+        }
+    }
+}
